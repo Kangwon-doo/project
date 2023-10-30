a023c48954d8c7ba3239ea28b11526a25bb00ed0
Clara <kim963@wisc.edu>
2023-10-30 20:53:10 +0900

    static files

---
+++ mysite/main/static/js/temp.js
@@ -0,0 +1,12 @@
+var lat = '12';
+var lon = 1212;
+
+console.log(10); 
+
+// <input> tag에 값을 넣음
+$('input[name=lat]').attr('value',lat);
+$('input[name=lon]').attr('value',lon);
+
+// document.location.submit();
+
+console.log(lat);    
