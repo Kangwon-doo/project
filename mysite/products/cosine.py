cc589a4c47a0085dd9826398617adc41d2bdffa7
Heewon-k <heewon.ms.kim@gmail.com>
2023-11-07 16:09:10 +0900

    modify cosine sim to adjust to new column names

--- mysite/products/cosine.py
+++ mysite/products/cosine.py
@@ -22,14 +22,13 @@ def most_similar(coffee_id, top_n=10):
 
 
 def cos_recommendation(user_input, top_n=5):
-    column_mapping = {'바디감': 'body', '신맛': 'sour', '단맛': 'sweet', '쓴맛': 'bitter', '타입_디카페인': 'caf', '타입_블렌드': 'blend',
-                      '향료_풍미': '향료', '달콤함': '달콤한', '고소함': '고소한'}  # 'notes'
+    column_mapping = {'바디감': 'body', '신맛': 'sour', '단맛': 'sweet', '쓴맛': 'bitter', '타입_디카페인': 'caf', '타입_블렌드': 'blend'}  # 'notes'
     df.rename(columns=column_mapping, inplace=True)
 
     # Create a new dictionary
     new_dict = user_input.copy()
     # Add other keys with initial values
-    new_keys = ['꽃', '과일', '달콤한', '허브', '고소한', '향료', '초콜릿']
+    new_keys = ['꽃', '과일', '달콤함', '허브', '고소함', '향료', '초콜릿']
     for key in new_keys:
         new_dict[key] = 0
     # Iterate through the 'notes' list and add new keys to 'new_dict'
@@ -39,7 +38,7 @@ def cos_recommendation(user_input, top_n=5):
     df.loc[len(df)] = new_dict
 
     features_df = df[
-        ['body', 'sour', 'sweet', 'bitter', 'caf', 'blend', '꽃', '과일', '허브', '달콤한', '고소한', '향료', '초콜릿']]  # '타입_싱글오리진'
+        ['body', 'sour', 'sweet', 'bitter', 'caf', 'blend', '꽃', '과일', '허브', '달콤함', '고소함', '향료', '초콜릿']]  # '타입_싱글오리진'
 
     embeddings = features_df.values
     cosine_similarity_matrix = cosine_similarity(embeddings, embeddings)
