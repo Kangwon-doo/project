88e3c7e56566ffb1139ded32dab5105edfc2bc07
Clara <kim963@wisc.edu>
2023-11-10 12:38:55 +0900

    change style.css name to test.css

--- mysite/config/settings.py
+++ mysite/config/settings.py
@@ -42,10 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django_filters',
     'common.apps.CommonConfig',
-    'crispy_forms',
-    'crispy_bootstrap4',
     'formtools',
-    'common',
+
     'django.contrib.sites',
     # allauth 관련 앱 목록 추가
     'allauth',
@@ -97,7 +95,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'wondoodoo', # DB name
         'USER': 'root', # account name
-        'PASSWORD': '1234',
+        'PASSWORD': '6618',
         'HOST': '127.0.0.1',  # 서버주소
         'PORT': '3306', # MySQL 포트 번호: 기본값 3306
 }
