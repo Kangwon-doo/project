0758c5004b70c698e2c153f7164eaa13838782c2
Clara <kim963@wisc.edu>
2023-11-23 18:03:20 +0900

    card

--- mysite/products/views.py
+++ mysite/products/views.py
@@ -142,7 +142,7 @@ def roastery_detail(request, roastery_id): #로스터리ID
     roastery_info = Roastery.objects.get(RoasteryID = roastery_id)
     coffees = Coffee.objects.filter(RoasteryID = roastery_id)
     
-    coffee_paginator = Paginator(coffees, 3)
+    coffee_paginator = Paginator(coffees, 9)
     page_num = request.GET.get('page')
 
     try:
