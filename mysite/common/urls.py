10e5018cb26b56a7900f74e4a8ecfdecadbbc229
rzse <heezj_i@naver.com>
2023-11-28 17:07:45 +0900

    modified

--- mysite/common/urls.py
+++ mysite/common/urls.py
@@ -13,7 +13,7 @@ app_name = 'common'
 
 
 urlpatterns = [
-    path('login/', auth_views.LoginView.as_view(template_name='common/login.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='main/mainpage.html'), name="logout"),
     path('signup/', views.signup, name='signup'),
     path('signup/test', views.signup_test, name='signup_test'),
